59578b40fcbdd9bd7599a6e24cb09bca5402c8e2
GojkoMaric <gojko.maric@yahoo.com>
2018-02-20 20:09:54 +0100

    galleries controller (index) + fix kernel/cors

--- app/Http/Controllers/GalleriesController.php
+++ app/Http/Controllers/GalleriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Gallery;
 
 class GalleriesController extends Controller
 {
@@ -13,7 +14,7 @@ class GalleriesController extends Controller
      */
     public function index()
     {
-        //
+        return Gallery::all();
     }
 
     /**
--- config/cors.php
+++ config/cors.php
@@ -10,10 +10,10 @@ return [
      | to accept any value.
      |
      */
-    'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
-    'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
-    'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
-    'exposedHeaders' => [],
-    'maxAge' => 0,
+    // 'supportsCredentials' => false,
+    // 'allowedOrigins' => ['*'],
+    // 'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
+    // 'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
+    // 'exposedHeaders' => [],
+    // 'maxAge' => 0,
 ];
